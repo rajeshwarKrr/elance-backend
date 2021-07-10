492ab0075027c9e9ae464c075ae544b8cec140a9
rajeshwarkrr <rajeshwarreddy.kadari@gmail.com>
2021-07-10 19:10:22 +0530

    chaged port keyword

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ async function startApolloServer() {
   await server.start();
   server.applyMiddleware({ app });
 
-  await new Promise(resolve => app.listen({port: process.env.port || 4000 }, resolve));
+  await new Promise(resolve => app.listen({port: process.env.PORT || 4000 }, resolve));
   console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`);
   return { server, app };
 }
