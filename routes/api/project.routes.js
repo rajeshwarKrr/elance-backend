245a8522575c09e87b93aa0d576edaadbf1f479b
Rajeshwar Reddy Kadari <rajeshwarreddy.kadari@gmail.com>
2021-07-28 16:06:56 +0530

    changed get to post

--- routes/api/project.routes.js
+++ routes/api/project.routes.js
@@ -3,9 +3,9 @@ const router = express.Router({ mergeParams : true });
 
 const projectControllers = require('../../controllers/projects.controllers')
 
-router.get('/getAllProjects', projectControllers.getAllProjects);
+router.post('/getAllProjects', projectControllers.getAllProjects);
 router.post('/createProject', projectControllers.createProject);
 router.post('/applyProject', projectControllers.applyProject);
-router.get('/getAllAppliedProjects', projectControllers.getAllAppliedProjects)
+router.post('/getAllAppliedProjects', projectControllers.getAllAppliedProjects)
 
 module.exports = router;
