026a2b561c8a7e896161dc1f17e1474c803700dc
Rajeshwar Reddy Kadari <rajeshwarreddy.kadari@gmail.com>
2021-07-28 16:06:18 +0530

    updated get to post

--- routes/api/user.routes.js
+++ routes/api/user.routes.js
@@ -3,11 +3,11 @@ const userControllers = require('../../controllers/users.controllers')
 
 const router = express.Router({ mergeParams : true });
 
-router.get('/getAllUsers', userControllers.getAllUsers);
+router.post('/getAllUsers', userControllers.getAllUsers);
 router.post('/getUserByEmail', userControllers.findByEmail);
 router.post('/registerUser', userControllers.registerUser);
 router.post('/setReview', userControllers.setReview);
-router.get('/getUserReviews', userControllers.getUserReviews);
+router.post('/getUserReviews', userControllers.getUserReviews);
 router.post("/readNotification", userControllers.readNotification);
 
 module.exports = router
