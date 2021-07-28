31cb30193fb8f91c8c2fc3a3dbd157e3e0fb814a
Rajeshwar Reddy Kadari <rajeshwarreddy.kadari@gmail.com>
2021-07-28 22:07:38 +0530

    updated express json

--- server.js
+++ server.js
@@ -16,8 +16,8 @@ corsOptions = {
 
 dotenv.config()
 app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.use( cors( corsOptions ) );
 
