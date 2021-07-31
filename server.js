345a2bac5d433d76737f437ffef43dc26ebf44cb
Rajeshwar Reddy Kadari <rajeshwarreddy.kadari@gmail.com>
2021-08-01 01:36:43 +0530

    updated error handling
    --previous commit went wrong

--- server.js
+++ server.js
@@ -24,20 +24,20 @@ connectDB()
   .then(() => console.log("Database Connected"))
   .catch((err) => console.log(err))
 
-app.use((req, res, next) => {
-  const error = new Error("Not found");
-  error.status = 404;
-  next(error);
-});
+// app.use((req, res, next) => {
+//   const error = new Error("Not found");
+//   error.status = 404;
+//   next(error);
+// });
 
-// error handler middleware
-app.use((error, req, res, next) => {
-  res.status(error.status || 500).send({
-    error: {
-      status: error.status || 500,
-      message: error.message || 'Internal Server Error',
-    },
-  });
+// // error handler middleware
+// app.use((error, req, res, next) => {
+//   res.status(error.status || 500).send({
+//     error: {
+//       status: error.status || 500,
+//       message: error.message || 'Internal Server Error',
+//     },
+//   });
 });
 
 app.get("/", (req, res) => {
